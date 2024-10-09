d18728ce11564887063775d20679609139f48a77
shimozukuri <whitelord228@yandex.ru>
2024-10-10 01:03:53 +0300

    first commit

---
+++ src/main/java/project/shimozukuri/datastoremicroservice/DataStoreMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package project.shimozukuri.datastoremicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStoreMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStoreMicroserviceApplication.class, args);
+    }
+
+}
